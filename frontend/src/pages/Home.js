83c40bb6532d670f355de2dcbef2a6ed1b091e6d
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-15 01:57:29 -0400

    Fetched data with new UI components/CSS

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react'
+
+//components
+import WorkoutDetails from '../components/WorkoutDetails'
+
+
 const Home = () =>{
+
+    const[workouts, setWorkouts] = useState(null)     
+    
+    useEffect(() => {  //this fires a function when the component is rendered. [] (dependency array)-> fires once 
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts') // to solve cors error (added proxy in pacakage-json)
+            const json = await response.json()
+
+            if(response.ok){
+                setWorkouts(json)  // setting state to response if the response is okay
+            }
+        }
+        fetchWorkouts()
+    },[])
+    
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {workouts && workouts.map((workout) =>(  //if there is workouts then run map
+                    <WorkoutDetails key={workout._id} workout={workout} />  //passing workout as prop to the WorkoutDetails component
+                ))}
+            </div>
         </div>
     )
 }
