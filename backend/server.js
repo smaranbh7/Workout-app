85aec6185b4071140a14d5c10223c9493269802e
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-21 10:15:01 -0400

    User routes, contoller & model

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose= require('mongoose')
 const workoutRoutes=require('./routes/workouts')
+const userRoutes=require('./routes/user')
 
 //express app
 const app = express()
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 //routes
 // This grabs and attaches all the routes from workouts.js and uses in the app. And when wwe go to this: '/api/workouts' then workoutRoutes's routes gets fired and to end of it
 app.use('/api/workouts', workoutRoutes) 
+app.use('/api/user', userRoutes) 
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
