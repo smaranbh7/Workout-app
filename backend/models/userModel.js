689625ef28774665b0784987618ed6ed373d8385
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-22 06:22:26 -0400

    comparing tokens and logging users in

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -36,13 +36,34 @@ userSchema.statics.signup = async function(email, password) {
   if (exists) {
     throw Error('Email already in use')
   }
-
+   //Password hashing
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
   const user = await this.create({ email, password: hash })
 
   return user
+}
+
+
+userSchema.statics.login = async function(email, password) {
+
+    if (!email || !password) {
+      throw Error('All fields must be filled')
+    }
+  
+    const user = await this.findOne({ email })
+    if (!user) {
+      throw Error('Incorrect email')
+    }
+  
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) {
+      throw Error('Incorrect password')
+    }
+  
+    return user
+
 }
 
 module.exports = mongoose.model('User', userSchema)
