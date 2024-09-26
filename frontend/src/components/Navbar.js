378c7ec5f343a920407099601dc1a4229ebc7ded
Smaran Bhattarai <smaran10.bhattarai@gmail.com>
2024-09-26 08:43:16 -0400

    initial auth status set

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const NavBar = () =>{
     const { logout } = useLogout()
+    const { user } = useAuthContext()
+
     const handleClick = ()=> {
         logout()
     }
@@ -13,13 +16,18 @@ const NavBar = () =>{
             <h1>Workout Buddy</h1>
             </Link>
             <nav>
+                {user && ( // output the tempelate when we have the user
                 <div>
+                    <span>{user.email}</span>
                     <button onClick={handleClick}>Log out </button>
                 </div>
+                )}
+                {!user && ( //output if we not have the user 
                 <div>
                     <Link to ="/login">Login</Link>
                     <Link to ="/signup">Signup</Link>
                 </div>
+                )}
             </nav>
         </div>
     </header>
